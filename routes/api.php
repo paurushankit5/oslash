3bb155bd68fc2a95e987e63e3a8cdaaf758863ff
Paurush Ankit <paurush.ankit@freighttiger.com>
2020-12-07 01:37:20 +0530

    route change

--- routes/api.php
+++ routes/api.php
@@ -48,7 +48,7 @@ Route::prefix('superadmin')->middleware(['auth:api','checkSuperAdmin'])->group(f
 
 
 Route::post('/register', 'App\Http\Controllers\API\AuthController@register');
-Route::post('/login', 'App\Http\Controllers\API\AuthController@login');
-Route::get('/pleaelogin', function(){
+Route::post('/loginuser', 'App\Http\Controllers\API\AuthController@login');
+Route::get('/pleaselogin', function(){
         return 'Please login';
 })->name('login');
